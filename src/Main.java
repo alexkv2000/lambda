f2b17aaba8f95b5866e84bb90563a82e6f6127c1
Alexxey <AlexKv2000@mail.ru>
2022-05-17 18:56:41 +0300

    correct Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Worker.OnTaskDoneListener listener = System.out::println;
+        OnTaskDoneListener listener = System.out::println;
         OnTaskErrorListener taskErr = x -> (x == 33) ? 0 : 1; // значение 0 - ошибка; 1 - корректно.
 
         Worker worker = new Worker(listener, taskErr);
